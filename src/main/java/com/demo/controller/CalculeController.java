81f607d9e0040d04c56e29406b375c771ba58cbe
khalil <khalillakhdharatc@gmail.com>
2023-03-26 14:16:20 +0100

    Create CalculeController.java

---
+++ src/main/java/com/demo/controller/CalculeController.java
@@ -0,0 +1,24 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculeController {
+
+	@PostMapping("/somme")
+	public String somme(@RequestParam int a,@RequestParam int b)
+	{
+		int somme=a+b;
+		return "la somme des deux entier est: "+somme;
+	}
+	@PostMapping("produit")
+	public String produit(@RequestParam int a,@RequestParam int b)
+	{
+		int produit=a*b;
+		return "le produit des deux entier est: "+produit;
+	}
+	
+	
+}
